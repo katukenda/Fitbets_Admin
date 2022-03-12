de6e1de80f68dc5f6ce45321072e4d9352dfc908
katukenda <janithaprashadkatukenda@gmail.com>
2022-03-12 17:38:29 +0530

    admin registeration crerate

---
+++ fitbets admin/Constants/Constant.swift
@@ -0,0 +1,16 @@
+//
+//  Constant.swift
+//  fitbets admin
+//
+//  Created by Janitha Katukenda on 2022-03-11.
+//
+
+import Foundation
+
+let app_id = ""
+let rest_key = ""
+
+let device_token = "aaaaaaa"
+let base_url = "http://ec2-35-83-63-15.us-west-2.compute.amazonaws.com:8000/adminnew"
+let login_url = "\(base_url)/login"
+let register_url = "\(base_url)/register"
---
+++ fitbets admin/Model/RegisterModel.swift
@@ -0,0 +1,17 @@
+//
+//  RegisterModel.swift
+//  fitbets admin
+//
+//  Created by Janitha Katukenda on 2022-03-11.
+//
+
+import Foundation
+
+struct RegisterModel: Encodable {
+    
+    let admin_name: String
+    let email_address: String
+    let mobile_number: String
+    let password: String
+    let device_token: String
+}
